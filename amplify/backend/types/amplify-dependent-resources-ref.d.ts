9c730fe9489d28282b702333406561419dc0c621
EC2 Default User <ec2-user@ip-172-31-8-224.ap-northeast-1.compute.internal>
2022-09-19 16:37:00 +0000

    Added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifymemoappstudy5c4a54c2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
