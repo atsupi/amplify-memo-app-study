fcb6e10e9b112f9540f2c9170c6b89b9e4613db5
EC2 Default User <ec2-user@ip-172-31-8-224.ap-northeast-1.compute.internal>
2022-09-19 18:39:31 +0000

    Added notes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifymemoappstudy": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
